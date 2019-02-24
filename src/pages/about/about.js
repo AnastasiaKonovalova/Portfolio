7a33b69dc0c7e4a5f6e8717481219407c5a1a631
AnastasiaKonovalova <silentium901@mail.ru>
2019-02-24 20:08:58 +0300

    components footer, header added

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,3 +1,8 @@
+import 'normalize.css';
+
 import '../common.scss';
 import './about.scss';
-import 'normalize.css';
+import '../../components/header/header.scss';
+import '../../components/footer/footer.scss';
+
+console.log('about.js')
--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,5 +1,7 @@
 import '../common.scss';
 import 'normalize.css';
 import './blog.scss';
+import '../../components/header/header.scss';
+import '../../components/footer/footer.scss';
 
 console.log('blog.js');

e747bf65240632623a12d3e6f83c2a4ad9aab138
AnastasiaKonovalova <silentium901@mail.ru>
2019-03-14 11:17:07 +0300

    repaired adaptive for horizontal mobile

--- src/utilities/commonEvents.js
+++ src/utilities/commonEvents.js
@@ -6,11 +6,11 @@ export const initNavigationListeners = (firstSection) => {
 
     const showNavDisplay = () => {
         fullscreenNav.classList.add('fullscreen--visible');
-        // document.body.style.overflow = 'hidden';
+        document.body.style.overflow = 'hidden';
     };
     const hideNavDisplay = () => {
         fullscreenNav.classList.remove('fullscreen--visible');
-        // document.body.style.overflow = 'auto';
+        document.body.style.overflow = 'auto';
     };
     
     showNavButton.addEventListener('click', showNavDisplay);

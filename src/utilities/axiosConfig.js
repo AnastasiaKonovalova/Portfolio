f11e7ad406471c5ad3760464e3abed6cbdbc3369
AnastasiaKonovalova <silentium901@mail.ru>
2019-04-19 10:12:37 +0400

    new axios config

--- src/utilities/axiosConfig.js
+++ src/utilities/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/';
 export const apiRequest = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: baseURL,
   mode: 'cors'
 });
